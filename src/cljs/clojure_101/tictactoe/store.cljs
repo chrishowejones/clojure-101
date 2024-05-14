11f2fec46df9694f73cfee8560123077d7bd034d
Chris Howe-Jones <chrishowe-jones@F124236Y7M.local>
2024-05-14 10:09:35 +0100

    Prevent current-turn reloading

--- src/cljs/clojure_101/tictactoe/store.cljs
+++ src/cljs/clojure_101/tictactoe/store.cljs
@@ -10,12 +10,12 @@
 ;; --------------------------------------------------------
 
 (defonce app-state (reagent/atom (game/new-game)))
-(def current-turn (reaction (game/current-turn @app-state)))
+(defonce current-turn (reaction (game/current-turn @app-state)))
 (defn send-event! [e] (swap! app-state game/handle-event e))
 
 (comment
 
   @app-state
 
-  (send-event! :restart)
+  (send-event! [2 0])
   )
